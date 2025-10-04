be0b323f923c792e75e9eba6c3a3f0fc6e58e089
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2025-10-04 13:23:52 +0200

    feat: use environment variable for API base URL;

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,11 +1,12 @@
 import userAdapter from "../adapters/userAdapter";
 
-const API_URL = "http://localhost:3001/api/v1/user";
+const API_URL = import.meta.env.VITE_API_URL;
+const USER_API = `${API_URL}/user`;
 
 const userService = {
   // Connexion
   login: async (email, password) => {
-    const res = await fetch(`${API_URL}/login`, {
+    const res = await fetch(`${USER_API}/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
@@ -19,7 +20,7 @@ const userService = {
 
   // Récupération profil utilisateur
   getUser: async (token) => {
-    const response = await fetch(`${API_URL}/profile`, {
+    const response = await fetch(`${USER_API}/profile`, {
       method: "POST",
       headers: {
         Authorization: `Bearer ${token}`,
@@ -33,7 +34,7 @@ const userService = {
   // Mise à jour du profil utilisateur
   updateUser: async (token, firstName, lastName) => {
     const bodyData = userAdapter.userToAPI(firstName, lastName); // ✅ Adaptation ici
-    const response = await fetch(`${API_URL}/profile`, {
+    const response = await fetch(`${USER_API}/profile`, {
       method: "PUT",
       headers: {
         Authorization: `Bearer ${token}`,
